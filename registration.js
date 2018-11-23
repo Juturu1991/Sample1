fcf85a0e895290000762b39b5efdfd8bab7fe8bd
Juturu1991 <juturu50@gmail.com>
2018-11-24 00:06:14 +0530

    latest

--- registration.js
+++ registration.js
@@ -1,18 +1,60 @@
-window.onload = function () {
-    var btnCreate = document.getElementById('btnCreate');
-    btnCreate.onclick = register;
-    //btnCreate.addEventListener('click',register);   
-}
+/*
+Pure JS
+*/
+//window.onload = function () {
+//    var btnCreate = document.getElementById('btnCreate');
+//    btnCreate.onclick = register;
+//    //btnCreate.addEventListener('click',register);   
+//}
 
+//Document ready specified
+//$(document).ready(function(){
+//   $('#btnCreate').click(register);
+//})
+
+//Jquery Wrapper w/o doc ready
+$(function(){
+    $('#btnCreate').click(register);
+});
 
 var register = function () {
 
-    var _fName = document.getElementById('txtFName');
-    var _lName = document.getElementById('txtLName');
-    var _pass = document.getElementById('txtPass');
-    var _email = document.getElementById('txtEmail');
+    var _fName = $('#txtFName').val(); //document.getElementById('txtFName');
+    var _lName = $('#txtLName').val(); //document.getElementById('txtLName');
+    var _pass = $('#txtPass').val(); //document.getElementById('txtPass');
+    var _email = $('#txtEmail').val(); //document.getElementById('txtEmail');
 
+    if (_fName.trim() == "") {
+        alert("Please fill first name");
+        return;
+    }
 
+    if (_lName.trim() == "") {
+        alert("Please fill last name");
+        return;
+    }
+
+    if (_pass == "") {
+        alert("Please pass3ord");
+        return;
+    } else {
+        if (_pass.length < 7) {
+            alert("Password too short");
+            return;
+        } else {
+            if (!/\d/.test(_pass)) {
+                alert('Atleast number');
+                return;
+            }
+        }
+    }
+
+    if(!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(_email.val())){
+        alert("Email not valid");
+        return;
+    }
+
+    /* Pure Js
     if (_fName.value.trim() == "") {
         alert("Please fill first name");
         return;
@@ -42,6 +84,7 @@ var register = function () {
         alert("Email not valid");
         return;
     }
+    */
 
     alert('Created succesfuyly;)');
 }
